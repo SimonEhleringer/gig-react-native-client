9de0a6c7ef159461df22d284b84d1bbb86ede05b
SimonEhleringer <simon.ehleringer@tuer.de>
2021-02-22 13:00:50 +0100

    Add animation to tempo component

--- domain/song/Tempo.tsx
+++ domain/song/Tempo.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Animated, Easing } from 'react-native';
-import { Text, ListItem } from 'react-native-elements';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import { MARGIN_HALF, PADDING, PADDING_HALF } from '../../config/themes';
-import { useTheme } from '../../hooks/useTheme';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Animated, Easing } from "react-native";
+import { Text, ListItem } from "react-native-elements";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import { MARGIN_HALF, PADDING, PADDING_HALF } from "../../config/themes";
+import { useTheme } from "../../hooks/useTheme";
 
 interface TempoProps {
   tempo: number;
@@ -37,118 +37,45 @@ function interval(this: any, duration: number, fn: () => void) {
   };
 }
 
-// class interval2 {
-//   baseline: number | undefined = undefined;
-//   duration: number;
-//   fn: () => void;
-//   timer: any;
-
-//   constructor(duration: number, fn: () => void) {
-//     this.duration = duration;
-//     this.fn = fn;
-//     //this.timer = () => {};
-//   }
-
-//   run() {
-//     if (this.baseline === undefined) {
-//       this.baseline = new Date().getTime();
-//     }
-//     this.fn();
-//     var end = new Date().getTime();
-//     this.baseline += this.duration;
-
-//     var nextTick = this.duration - (end - this.baseline);
-//     if (nextTick < 0) {
-//       nextTick = 0;
-//     }
-
-//     this.timer = setTimeout(function () {
-//       this.run(end);
-//     }, nextTick);
-//   }
-
-// }
-
 const Tempo: React.FC<TempoProps> = ({ tempo, isMetronomeOn }) => {
   const theme = useTheme();
-
-  //const [timer, setTimer] = useState(new NanoTimer());
 
   const [xValue, setXValue] = useState(new Animated.Value(0));
   const [isLeft, setIsLeft] = useState(true);
 
+  const [time, setTime] = useState(60000 / tempo);
+
   useEffect(() => {
-    // const interval = setInterval(() => {
-    //   setIsLeftIndicatorActive(!isLeftIndicatorActive);
-    // }, 60000 / (tempo * 1.1));
-    // return () => {
-    //   clearInterval(interval);
-    //
-
-    //};
-
-    const time = 60000 / tempo;
-
-    // const interval = setInterval(() => {
-    //   setIsLeft((left) => !left);
-    // }, time);
-
-    // return () => {
-    //   clearInterval(interval);
-    // };
+    // Only activate metronome, if wanted
+    if (!isMetronomeOn) {
+      return;
+    }
 
     const timer = new (interval as any)(time, () => {
-      setIsLeft((left) => !left);
+      setIsLeft((prev) => !prev);
     });
 
     timer.run();
-
     return () => {
       timer.stop();
     };
   }, []);
 
-  // useEffect(() => {
-  //   if (isMetronomeOn) {
-  //     moveAnimation();
-  //   } else {
-  //     xValue.stopAnimation();
-  //   }
-  // }, [isMetronomeOn]);
-
-  const moveAnimation = () => {
-    // Animated.loop(
-    //   Animated.sequence([
-    //     Animated.timing(xValue, {
-    //       toValue: 23,
-    //       duration: 100,
-    //       useNativeDriver: false,
-    //       easing: Easing.ease,
-    //       delay: (60000 / tempo - 100) * 0.98 * 0.98,
-    //     }),
-    //     Animated.timing(xValue, {
-    //       toValue: 0,
-    //       duration: 100,
-    //       useNativeDriver: false,
-    //       easing: Easing.ease,
-    //       delay: (60000 / tempo - 100) * 0.98 * 0.98,
-    //     }),
-    //   ])
-    // ).start();
-    //  Animated.timing(xValue, {
-    //    toValue:  ? 23 : 0,
-    //    duration: 100,
-    //    useNativeDriver: false,
-    //  }).start();
-  };
+  useEffect(() => {
+    Animated.timing(xValue, {
+      toValue: isLeft ? 23 : 0,
+      duration: time / 5,
+      useNativeDriver: false,
+    }).start();
+  }, [isLeft]);
 
   return (
     <View style={styles.container}>
       <View style={styles.bpmIndicator}>
-        <View
+        <Animated.View
           style={[
             styles.bpmIndicatorPoint,
-            { backgroundColor: theme.colors?.primary, left: isLeft ? 0 : 23 },
+            { backgroundColor: theme.colors?.primary, left: xValue },
           ]}
         />
       </View>
@@ -187,12 +114,12 @@ const Tempo: React.FC<TempoProps> = ({ tempo, isMetronomeOn }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   bpmIndicator: {
     margin: MARGIN_HALF,
-    flexDirection: 'row',
+    flexDirection: "row",
     width: 35,
   },
   bpmIndicatorPoint: {
--- screens/PlaylistSongsScreen.tsx
+++ screens/PlaylistSongsScreen.tsx
@@ -1,36 +1,36 @@
-import { RouteProp, useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import React, { useLayoutEffect, useRef } from 'react';
-import { Text } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import RBSheet from 'react-native-raw-bottom-sheet';
-import { useSelector } from 'react-redux';
-import { ReduxState } from '../config/store';
+import { RouteProp, useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import React, { useLayoutEffect, useRef, useState } from "react";
+import { Switch, Text, View } from "react-native";
+import { ListItem } from "react-native-elements";
+import RBSheet from "react-native-raw-bottom-sheet";
+import { useSelector } from "react-redux";
+import { ReduxState } from "../config/store";
 import {
   BORDER_RADIUS,
   BOTTOM_SHEET_HEADER_HEIGHT,
   BOTTOM_SHEET_LIST_ITEM_HEIGHT,
   PADDING,
-} from '../config/themes';
-import withBackground from '../domain/common/withBackground';
-import withBottomRoundedCorners from '../domain/common/withBottomRoundedCorners';
-import PlaylistSongListContainer from '../domain/playlist/playlistSong/PlaylistSongListContainer';
+} from "../config/themes";
+import withBackground from "../domain/common/withBackground";
+import withBottomRoundedCorners from "../domain/common/withBottomRoundedCorners";
+import PlaylistSongListContainer from "../domain/playlist/playlistSong/PlaylistSongListContainer";
 import {
   AddSongToPlaylistParams,
   PlaylistStackParamList,
-} from '../navigation/PlaylistStack';
-import { MaterialIcons as Icon } from '@expo/vector-icons';
-import { useTheme } from '../hooks/useTheme';
+} from "../navigation/PlaylistStack";
+import { MaterialIcons as Icon } from "@expo/vector-icons";
+import { useTheme } from "../hooks/useTheme";
 
 interface PlaylistSongsScreenProps {
-  route: RouteProp<PlaylistStackParamList, 'PlaylistSongs'>;
+  route: RouteProp<PlaylistStackParamList, "PlaylistSongs">;
 }
 
 const PlaylistSongsScreen: React.FC<PlaylistSongsScreenProps> = ({ route }) => {
   const theme = useTheme();
   const navigation: StackNavigationProp<
     PlaylistStackParamList,
-    'PlaylistSongs'
+    "PlaylistSongs"
   > = useNavigation();
 
   const bottomSheetRef = useRef<RBSheet>(null);
@@ -45,7 +45,7 @@ const PlaylistSongsScreen: React.FC<PlaylistSongsScreenProps> = ({ route }) => {
       title: playlist?.name,
       headerRight: () => (
         <Icon
-          name='more-vert'
+          name="more-vert"
           size={25}
           color={theme.colors?.black}
           style={{ paddingRight: PADDING }}
@@ -62,7 +62,7 @@ const PlaylistSongsScreen: React.FC<PlaylistSongsScreenProps> = ({ route }) => {
       playlistId: route.params.playlistId,
     };
 
-    navigation.navigate('AddSongToPlaylist', params);
+    navigation.navigate("AddSongToPlaylist", params);
   };
 
   return (
@@ -77,13 +77,31 @@ const PlaylistSongsScreen: React.FC<PlaylistSongsScreenProps> = ({ route }) => {
           },
         }}
         height={
-          BOTTOM_SHEET_LIST_ITEM_HEIGHT + BOTTOM_SHEET_HEADER_HEIGHT + PADDING
+          BOTTOM_SHEET_LIST_ITEM_HEIGHT * 2 +
+          BOTTOM_SHEET_HEADER_HEIGHT +
+          PADDING
         }
       >
         <ListItem onPress={handleAddSong}>
-          <Icon name='add' size={25} color='black' />
+          <Icon name="add" size={25} color="black" />
           <ListItem.Content>
             <ListItem.Title>Song hinzufügen</ListItem.Title>
+          </ListItem.Content>
+        </ListItem>
+
+        <ListItem>
+          <Icon name="add" size={25} color="black" />
+          <ListItem.Content>
+            <View
+              style={{
+                width: "100%",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <ListItem.Title>Auftritts-Modus</ListItem.Title>
+              <Switch />
+            </View>
           </ListItem.Content>
         </ListItem>
       </RBSheet>
